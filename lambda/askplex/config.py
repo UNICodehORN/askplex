923b9d6f90ae205a3cb3c4cb2841746647e8d913
hendrik92 <hendrik_horn@yahoo.de>
2026-08-08 00:21:06 +0200

    Re-add config.py

---
+++ lambda/askplex/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import logging
+
+SKILL_LOG_LEVEL=logging.INFO
+
+# Plex Media Server config
+PMS_SERVER_URL = 'https://'
+PMS_SERVER_TOKEN = ''
+PMS_DEFAULT_SECTION_NAME = ''
+PMS_DEFAULT_MAX_RESULTS = 100
+PMS_CLIENT_ACTIVE = False
+PMS_CLIENT_URL = 'https://'
+PMS_CLIENT_TIMEOUT = 3
